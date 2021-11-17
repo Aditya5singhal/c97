664e8a76ccf6bb86ee6c16fb5e6e17ac9765d492
Aditya5singhal <77671226+Aditya5singhal@users.noreply.github.com>
2021-11-17 20:59:31 +0530

    Add files via upload

---
+++ python classes/pocketmoney.py
@@ -0,0 +1,8 @@
+pocketmoney=int (input("what is your pocket money")) 
+print(pocketmoney)
+if(pocketmoney>500):
+    print("your pocket money is not enough")
+elif(pocketmoney<200):
+    print("your pocket money is very less")
+else:
+    print("yuor pocket money is very good")
